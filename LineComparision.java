56f5c37652a7e31826c28c919dc79f19c0f6ffea
Noor Fatma <nfatma8989@gmail.com>
2023-12-22 19:16:25 +0530

    Calculating length of a line

--- LineComparision.java
+++ LineComparision.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.linecomparision;
 
+import java.util.Scanner;
+
 public class LineComparision {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparision Computation Program!");
+		
+	public void CalculateLengthOfLine() {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter x1 value: ");
+		double x1=scanner.nextInt();
+		System.out.print("Enter x2 value: ");
+		double x2=scanner.nextInt();
+		System.out.print("Enter y1 value: ");
+		double y1=scanner.nextInt();
+		System.out.print("Enter y2 value: ");
+		double y2=scanner.nextInt();
+		double length= Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("The length of a Line is: " +length);
+		scanner.close();
 	}
 }
---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linecomparision;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation Program!");
+		
+		LineComparision lineComparision=new LineComparision();
+		System.out.println();
+		lineComparision.CalculateLengthOfLine();
+	}
+
+}
