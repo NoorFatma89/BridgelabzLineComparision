75356cf10c1e96349fa3fd54dc69700c6f54f428
Noor Fatma <83427246+NoorFatma89@users.noreply.github.com>
2023-12-26 12:53:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
 		LineComparision ln= new LineComparision();
 		Line1 line1 = new Line1();
 		Line2 line2 = new Line2();
-		System.out.println("1st Line Between Two Points= "+line1.line1);
-		System.out.println("2nd Line Between Two Points= "+line2.line2);
+		System.out.println("length of 1st Line= "+line1.line1);
+		System.out.println("length of 2nd Line = "+line2.line2);
 		ln.byEquals();
 		ln.byCompareTo();
 	}
