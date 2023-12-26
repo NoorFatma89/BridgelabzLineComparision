44918a539ed5d96c107f531b75fea4bf89ebb27a
Noor Fatma <nfatma8989@gmail.com>
2023-12-26 09:57:55 +0530

    using equals and compareTo method

---
+++ Line1.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linecomparision;
+
+public class Line1 {
+	double x1 = 5;  double y1 = 5;
+	double x2 = 10; double y2 = 10;
+	double line1 = Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+}
--- Main.java
+++ Main.java
@@ -2,14 +2,12 @@ package com.bridgelabz.linecomparision;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparision Computation Program!");
-		LineComparision lineComparision=new LineComparision();
-		System.out.println();
-		lineComparision.calculateLengthOfLine();
-		System.out.println();
-		lineComparision.checkLineEquality();
-		System.out.println();
-		lineComparision.compareTwoLines();
+		LineComparision ln= new LineComparision();
+		Line1 line1 = new Line1();
+		Line2 line2 = new Line2();
+		System.out.println("1st Line Between Two Points= "+line1.line1);
+		System.out.println("2nd Line Between Two Points= "+line2.line2);
+		ln.byEquals();
+		ln.byCompareTo();
 	}
-
 }
