0cf3c78e4bad77e7b05a98110184a81a92143410
Noor Fatma <nfatma8989@gmail.com>
2023-12-26 09:40:50 +0530

    Comparing two Lines

--- LineComparision.java
+++ LineComparision.java
@@ -3,53 +3,89 @@ package com.bridgelabz.linecomparision;
 import java.util.Scanner;
 
 public class LineComparision {
-		
-	public void CalculateLengthOfLine() {
+	int x1,x2,y1,y2,x3,x4,y3,y4;
+	
+	public void takingInput() {
 		Scanner scanner=new Scanner(System.in);
 		System.out.print("Enter x1 value: ");
-		int x1=scanner.nextInt();
+		x1=scanner.nextInt();
 		System.out.print("Enter x2 value: ");
-		int x2=scanner.nextInt();
+		x2=scanner.nextInt();
 		System.out.print("Enter y1 value: ");
-		int y1=scanner.nextInt();
+		y1=scanner.nextInt();
 		System.out.print("Enter y2 value: ");
-		int y2=scanner.nextInt();
-		double length= Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("The length of a Line is: " +length);
-		scanner.close();
+		y2=scanner.nextInt();
 	}
 	
-	public void checkLineEquality() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter values of Line 1");
-        System.out.print("Enter x1 value: ");
-        int x1 = scanner.nextInt();
-        System.out.print("Enter x2 value: ");
-        int x2 = scanner.nextInt();
-        System.out.print("Enter y1 value: ");
-        int y1 = scanner.nextInt();
-        System.out.print("Enter y2 value: ");
-        int y2 = scanner.nextInt();
+	public void takingInput2() {
+		Scanner scanner=new Scanner(System.in);
 
-        Double lengthOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-
-        System.out.println("Enter values of Line 2");
         System.out.print("Enter x3 value: ");
-        int x3 = scanner.nextInt();
+        x3 = scanner.nextInt();
         System.out.print("Enter x4 value: ");
-        int x4 = scanner.nextInt();
+        x4 = scanner.nextInt();
         System.out.print("Enter y3 value: ");
-        int y3 = scanner.nextInt();
+        y3 = scanner.nextInt();
         System.out.print("Enter y4 value: ");
-        int y4 = scanner.nextInt();
-
-        Double lengthOfLine2 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
-
-        if (lengthOfLine1.equals(lengthOfLine2))
+        y4 = scanner.nextInt(); 
+	}
+	
+	public double lengthCalculator(double p1, double q1,double p2, double q2) {
+		return (Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(q2 - q1, 2)));
+	}
+//	Calculating length of a line	
+	public void calculateLengthOfLine() {
+	    System.out.println("To calculate Length of a Line -");
+		takingInput();
+		double length=lengthCalculator(x1,y1,x2,y2);
+		System.out.println("The length of a Line is: " +length);
+	}
+	
+// 	Checking two line are equal or not
+	public void checkLineEquality() {
+	    System.out.println("To check two line are Equal or not by equals Method -");
+	    
+		System.out.println("Enter values of Line 1 ");
+		takingInput();
+		double lengthOfline1=lengthCalculator(x1,y1,x2,y2);
+		Double doubleLengthOfline1 = lengthOfline1;
+		
+		System.out.println("Enter values of Line 2");
+		takingInput2();
+		double lengthOfline2= lengthCalculator(x3,x4,y3,y4);
+		Double doubleLengthOfline2 = lengthOfline2;
+        
+        
+        if (doubleLengthOfline1.equals(doubleLengthOfline2))
             System.out.println("Lines are equal");
         else
             System.out.println("Lines are not equal");
-		scanner.close();        
     }
 	
+//	Comparing Two lines 
+	public void compareTwoLines() {
+	    System.out.println("To check line equality by compareTo Method  -");
+		System.out.println("Enter values of Line 1 ");
+		takingInput();
+		double lengthOfline1=lengthCalculator(x1,y1,x2,y2);
+		Double doubleLengthOfline1 = lengthOfline1;
+		
+		System.out.println("Enter values of Line 2");
+		takingInput2();
+		double lengthOfline2= lengthCalculator(x3,x4,y3,y4);
+		Double doubleLengthOfline2 = lengthOfline2;
+		
+		
+        int value=doubleLengthOfline1.compareTo(doubleLengthOfline2);
+        
+        if (value<0)
+            System.out.println("First Line is less than second Line ");
+        else if(value>0)
+        	System.out.println("First Line is greater than second Line");
+        else
+            System.out.println("Both Lines are equal");
+	}
+	
 }
+
+
--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.linecomparision;
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation Program!");
-		
 		LineComparision lineComparision=new LineComparision();
 		System.out.println();
-		lineComparision.CalculateLengthOfLine();
+		lineComparision.calculateLengthOfLine();
 		System.out.println();
 		lineComparision.checkLineEquality();
+		System.out.println();
+		lineComparision.compareTwoLines();
 	}
 
 }
