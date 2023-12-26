fc881b98d45e46f2f2165da2f51701391f20d4ca
Noor Fatma <83427246+NoorFatma89@users.noreply.github.com>
2023-12-26 13:08:21 +0530

    Update Line2.java

--- Line2.java
+++ Line2.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.linecomparision;
 
+//Declaring First Line and calculating length of that Line
 public class Line2 {
-	double m1 = 15;  double n1 = 15;
-	double m2 = 25; double n2 = 25;
-	double line2 = Math.sqrt(((m2-m1)*(m2-m1)) + ((n2-n1)*(n2-n1)));
+	double m1 = 6;  double n1 = 9;
+	double m2 = 23; double n2 = 12;
+	double length2 = Math.sqrt(((m2-m1)*(m2-m1)) + ((n2-n1)*(n2-n1)));
 }
